2a8ee12ea0273e49bc0d79f52aacc2319ad596a5
Samir Santos <santossamir@hotmail.com>
2022-03-10 12:57:45 -0300

    Estruturando projeto com pages, routes e components.

---
+++ app-filmaria/src/AppPrincipal.js
@@ -0,0 +1,10 @@
+
+import Routes from './routes';
+
+export default function AppPrincipal() {
+  return (
+    <div className="app">
+        <Routes/>
+    </div>
+  );
+}
---
+++ app-filmaria/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppPrincipal from './AppPrincipal';
+import './css/AppStyle.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AppPrincipal />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
