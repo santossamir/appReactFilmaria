5bbc8e5d22afd0cfc24fb51757ba65a490dcadd5
Samir Santos <santossamir@hotmail.com>
2022-03-11 19:06:27 -0300

    Adicionando a API na page home.

--- app-filmaria/src/pages/Home/index.jsx
+++ app-filmaria/src/pages/Home/index.jsx
@@ -1,8 +1,36 @@
+import api from '../../services/api';
+import { useEffect, useState } from 'react';
+import {Link} from 'react-router-dom';
+import "./home.css";
 
 export default function Home() {
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(()=>{
+
+      async function loadFilmes(){
+          const response = await api.get('r-api/?api=filmes')
+          setFilmes(response.data);
+      }
+
+      loadFilmes();
+
+  }, []);
+
+
     return (
-      <div>
-          <h1>Pagina Home</h1>
+      <div className='container'>
+          <div className='lista-filmes'>
+            {filmes.map((item)=>{
+              return(
+                <article key={item.id}>
+                   <strong>{item.nome}</strong>
+                   <img src={item.img} alt={item.nome}/>  
+                   <Link to="/">Acessar</Link>
+                </article>
+              )
+            })}
+          </div>
       </div>
     );
   }
