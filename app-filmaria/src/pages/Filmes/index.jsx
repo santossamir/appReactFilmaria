95ca8780527a198e849ea39abd506a8f94181f1e
Samir Santos <santossamir@hotmail.com>
2022-03-12 17:32:29 -0300

    Estilizando e desenvolvendo a page da Sinopse.Tambem fizemos um history para redirecionar link errado para a home.

--- app-filmaria/src/pages/Filmes/index.jsx
+++ app-filmaria/src/pages/Filmes/index.jsx
@@ -1,22 +1,32 @@
-import { useEffect, useState } from 'react';
-import {useParams}from 'react-router-dom';
+import {useEffect, useState} from 'react';
+import {useParams, useHistory}from 'react-router-dom';
 import './filme-info.css';
 import api  from '../../services/api';
 
 export default function Filme(){
 
     const {id} = useParams();
+    const history = useHistory();
+
     const [filmes, setFilmes] = useState([]);
     const [loading, setLoading] = useState(true);
 
     useEffect(()=> {
         async function loadFilmes(){
             const response = await api.get(`r-api/?api=filmes/${id}`);
+
+            if(response.data.length === 0){
+               history.replace('/');
+               return; 
+            }
+        
             setFilmes(response.data);
             setLoading(false);
         }
+
         loadFilmes();
-    }, [id]);
+
+    }, [history, id]);
 
     if(loading == true){
         return(
@@ -27,6 +37,16 @@ export default function Filme(){
     }
     return(
         <div className='filme-info'>
+            <h1>{filmes.nome}</h1>
+            <img src={filmes.foto} alt={filmes.nome}/>
+            <h3>Sinopse</h3>
+            {filmes.sinopse}
+            <div>
+                <button onClick={()=>{}} >Salver</button>
+                <button>
+                    <a target="blank" href={`https://youtube.com/results?search_query=${filmes.nome} Trailer`}>Trailer</a>
+                </button>
+            </div>
             
         </div>
     )
