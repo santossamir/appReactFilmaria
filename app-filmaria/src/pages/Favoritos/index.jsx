eff2b32ae3c82a668d81c1b10ebdd8d791314469
Samir Santos <santossamir@hotmail.com>
2022-03-14 11:19:39 -0300

    Estilizando a page Favoritos e cliando a logica do button Excluir com o metodo filter() do JS.

--- app-filmaria/src/pages/Favoritos/index.jsx
+++ app-filmaria/src/pages/Favoritos/index.jsx
@@ -1,7 +1,43 @@
+import { useEffect, useState} from "react";
+import {Link} from 'react-router-dom';
+import './favoritos.css';
+
 export default function Favoritos(){
+
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(()=>{
+        const minhaLista = localStorage.getItem('filmes');
+        setFilmes(JSON.parse(minhaLista) || []);
+    }, []);
+
+    function handleDelete(id){
+        let filtroFilmes = filmes.filter((item)=>{
+            return(item.id !== id)
+        })
+
+        setFilmes(filtroFilmes);
+        localStorage.setItem('filmes', JSON.stringify(filtroFilmes))
+    }
+
     return(
-        <div>
-            <h1>Pagina Filmes favoritos.</h1>
+        <div id="meus-filmes">
+            <h1>Mues Filmes</h1>
+            <ul>
+                {filmes.map((item) => {
+                    return(
+                        <li key={item.id}>
+                            <span>{item.nome}</span>
+
+                            <div>
+                                <Link to={`/filmes/${item.id}`}>Ver detalhes</Link>
+                                <button onClick={() => handleDelete(item.id)}>Excluir</button>
+                            </div>
+
+                        </li>
+                    )
+                })}
+            </ul>
         </div>
     )
 }
